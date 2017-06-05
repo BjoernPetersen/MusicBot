7191005d23b5abfbdd015ed6fd7ee8f6608a4b41
Björn Petersen <pheasn@gmail.com>
2017-06-05 17:29:00 +0200

    Implement toString, equals and hashCode in User

--- src/main/java/com/github/bjoernpetersen/jmusicbot/user/User.java
+++ src/main/java/com/github/bjoernpetersen/jmusicbot/user/User.java
@@ -125,4 +125,39 @@ public final class User {
   void invalidate() {
     this.invalid = true;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    User user = (User) o;
+
+    if (invalid != user.invalid) {
+      return false;
+    }
+    return name.equals(user.name);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = (invalid ? 1 : 0);
+    result = 31 * result + name.hashCode();
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+        "invalid=" + invalid +
+        ", name='" + name + '\'' +
+        ", hash='" + hash + '\'' +
+        ", uuid='" + uuid + '\'' +
+        ", permissions=" + permissions +
+        '}';
+  }
 }
