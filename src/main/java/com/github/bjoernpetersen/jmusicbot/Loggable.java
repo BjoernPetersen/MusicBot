31171c6301fc2fcb87b362e7482b2670376d2762
Björn Petersen <pheasn@gmail.com>
2017-07-01 13:40:10 +0200

    Fix calling class / method for log calls

--- src/main/java/com/github/bjoernpetersen/jmusicbot/Loggable.java
+++ src/main/java/com/github/bjoernpetersen/jmusicbot/Loggable.java
@@ -1,5 +1,7 @@
 package com.github.bjoernpetersen.jmusicbot;
 
+import java.util.Arrays;
+import java.util.Spliterator;
 import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,102 +17,142 @@ public interface Loggable {
   }
 
   default void logSevere(String msg) {
-    getLogger().severe(msg);
+    log(Level.SEVERE, msg);
   }
 
   default void logSevere(String msg, Throwable throwable) {
-    getLogger().log(Level.SEVERE, msg, throwable);
+    log(Level.SEVERE, msg, throwable);
   }
 
   default void logWarning(String msg) {
-    getLogger().warning(msg);
+    log(Level.WARNING, msg);
   }
 
   default void logWarning(String msg, Throwable throwable) {
-    getLogger().log(Level.WARNING, msg, throwable);
+    log(Level.WARNING, msg, throwable);
   }
 
   default void logInfo(String msg) {
-    getLogger().info(msg);
+    log(Level.INFO, msg);
   }
 
   default void logConfig(String msg) {
-    getLogger().config(msg);
+    log(Level.CONFIG, msg);
   }
 
   default void logFine(String msg) {
-    getLogger().fine(msg);
+    log(Level.FINE, msg);
   }
 
   default void logFiner(String msg) {
-    getLogger().finer(msg);
+    log(Level.FINER, msg);
   }
 
   default void logFinest(String msg) {
-    getLogger().finest(msg);
+    log(Level.FINEST, msg);
   }
 
   default void logSevere(String msg, Object... formatArgs) {
-    getLogger().severe(String.format(msg, formatArgs));
+    log(Level.SEVERE, msg, formatArgs);
   }
 
   default void logSevere(Throwable throwable, String msg, Object... formatArgs) {
-    getLogger().log(Level.SEVERE, String.format(msg, formatArgs), throwable);
+    log(Level.SEVERE, throwable, msg, formatArgs);
   }
 
   default void logWarning(String msg, Object... formatArgs) {
-    getLogger().warning(String.format(msg, formatArgs));
+    log(Level.WARNING, msg, formatArgs);
   }
 
   default void logWarning(Throwable throwable, String msg, Object... formatArgs) {
-    getLogger().log(Level.WARNING, String.format(msg, formatArgs), throwable);
+    log(Level.WARNING, throwable, msg, formatArgs);
   }
 
   default void logInfo(String msg, Object... formatArgs) {
-    getLogger().info(String.format(msg, formatArgs));
+    log(Level.INFO, msg, formatArgs);
   }
 
   default void logConfig(String msg, Object... formatArgs) {
-    getLogger().config(String.format(msg, formatArgs));
+    log(Level.CONFIG, msg, formatArgs);
   }
 
   default void logFine(String msg, Object... formatArgs) {
-    getLogger().fine(String.format(msg, formatArgs));
+    log(Level.FINE, msg, formatArgs);
   }
 
   default void logFiner(String msg, Object... formatArgs) {
-    getLogger().finer(String.format(msg, formatArgs));
+    log(Level.FINER, msg, formatArgs);
   }
 
   default void logFinest(String msg, Object... formatArgs) {
-    getLogger().finest(String.format(msg, formatArgs));
+    log(Level.FINEST, msg, formatArgs);
   }
 
   default void logSevere(Supplier<String> msgSupplier) {
-    getLogger().severe(msgSupplier);
+    log(Level.SEVERE, msgSupplier);
   }
 
   default void logWarning(Supplier<String> msgSupplier) {
-    getLogger().warning(msgSupplier);
+    log(Level.WARNING, msgSupplier);
   }
 
   default void logInfo(Supplier<String> msgSupplier) {
-    getLogger().info(msgSupplier);
+    log(Level.INFO, msgSupplier);
   }
 
   default void logConfig(Supplier<String> msgSupplier) {
-    getLogger().config(msgSupplier);
+    log(Level.CONFIG, msgSupplier);
   }
 
   default void logFine(Supplier<String> msgSupplier) {
-    getLogger().fine(msgSupplier);
+    log(Level.FINE, msgSupplier);
   }
 
   default void logFiner(Supplier<String> msgSupplier) {
-    getLogger().finer(msgSupplier);
+    log(Level.FINER, msgSupplier);
   }
 
   default void logFinest(Supplier<String> msgSupplier) {
-    getLogger().finest(msgSupplier);
+    log(Level.FINEST, msgSupplier);
+  }
+
+  default void log(Level level, Supplier<String> msgSupplier) {
+    StackTraceElement caller = findCaller();
+    getLogger().logp(level, caller.getClassName(), caller.getMethodName(), msgSupplier);
+  }
+
+  default void log(Level level, String msg, Object... formatArgs) {
+    StackTraceElement caller = findCaller();
+    getLogger().logp(
+        level,
+        caller.getClassName(),
+        caller.getMethodName(),
+        String.format(msg, formatArgs)
+    );
+  }
+
+  default void log(Level level, Throwable throwable, String msg, Object... formatArgs) {
+    StackTraceElement caller = findCaller();
+    getLogger().logp(
+        level,
+        caller.getClassName(),
+        caller.getMethodName(),
+        String.format(msg, formatArgs),
+        throwable
+    );
+  }
+
+  static StackTraceElement findCaller() {
+    String className = Loggable.class.getName();
+    StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+    Spliterator<StackTraceElement> iterator = Arrays.spliterator(stackTrace, 1, stackTrace.length);
+    StackTraceElement[] elementRef = new StackTraceElement[1];
+    while (iterator.tryAdvance(element -> elementRef[0] = element)) {
+      StackTraceElement element = elementRef[0];
+      if (!element.getClassName().equals(className)) {
+        return element;
+      }
+    }
+    throw new IllegalStateException();
   }
 }
