9577e16e4f93e8a912a241e1ed4452cd2d327248
Björn Petersen <pheasn@gmail.com>
2017-07-16 13:02:03 +0200

    Document BroadcasterInitializer

--- src/main/java/com/github/bjoernpetersen/jmusicbot/BroadcasterInitializer.java
+++ src/main/java/com/github/bjoernpetersen/jmusicbot/BroadcasterInitializer.java
@@ -6,6 +6,16 @@ import javax.annotation.Nonnull;
 @FunctionalInterface
 public interface BroadcasterInitializer {
 
+  /**
+   * Initialized a UDP broadcaster. The broadcaster should broadcast the specified message every few
+   * seconds with the specified destination port and group address.
+   *
+   * @param port a port number
+   * @param groupAddress a UDP multicast group IPv4 address
+   * @param message a message to broadcast
+   * @return a broadcaster
+   * @throws InitializationException if the broadcaster can not be started
+   */
   @Nonnull
   Closeable initialize(int port, String groupAddress, String message)
       throws InitializationException;
