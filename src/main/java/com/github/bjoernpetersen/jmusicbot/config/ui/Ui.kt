f5c5479e2947ac71ab568cf2b47abc8fbeece2fc
Björn Petersen <pheasn@gmail.com>
2017-10-06 20:25:22 +0200

    Refine config UI

--- src/main/java/com/github/bjoernpetersen/jmusicbot/config/ui/Ui.kt
+++ src/main/java/com/github/bjoernpetersen/jmusicbot/config/ui/Ui.kt
@@ -2,55 +2,66 @@ package com.github.bjoernpetersen.jmusicbot.config.ui
 
 import com.github.bjoernpetersen.jmusicbot.config.Config
 
-interface ConfigValueConverter<in T : Config.Entry, U> {
-  fun getWithoutDefault(t: T): U
-  fun getDefault(t: T): U
-  fun getWithDefault(t: T): U
-  fun set(t: T, u: U)
+interface ConfigValueConverter<in T : Config.Entry, in S, out G> {
+  fun getWithoutDefault(t: T): G
+  fun getDefault(t: T): G
+  fun getWithDefault(t: T): G
+  fun set(t: T, u: S)
 }
 
-object DefaultBooleanConverter : ConfigValueConverter<Config.BooleanEntry, Boolean> {
+private object DefaultBooleanConverter :
+    ConfigValueConverter<Config.BooleanEntry, Boolean?, Boolean> {
+
   override fun getDefault(t: Config.BooleanEntry): Boolean = t.defaultValue
   override fun getWithoutDefault(t: Config.BooleanEntry): Boolean = t.value
   override fun getWithDefault(t: Config.BooleanEntry): Boolean = t.value
-  override fun set(t: Config.BooleanEntry, u: Boolean) = t.set(u)
+  override fun set(t: Config.BooleanEntry, u: Boolean?) = t.set(u)
 }
 
-object DefaultStringConverter : ConfigValueConverter<Config.StringEntry, String?> {
+private object DefaultStringConverter : ConfigValueConverter<Config.StringEntry, String?, String?> {
   override fun getDefault(t: Config.StringEntry): String? = t.defaultValue
   override fun getWithoutDefault(t: Config.StringEntry): String? = t.valueWithoutDefault
   override fun getWithDefault(t: Config.StringEntry): String? = t.value
   override fun set(t: Config.StringEntry, u: String?) = t.set(u)
 }
 
-sealed class UiNode<in T : Config.Entry, U>(val converter: ConfigValueConverter<T, U>)
-class TextBox() : UiNode<Config.StringEntry, String?>(DefaultStringConverter)
-class PasswordBox() : UiNode<Config.StringEntry, String?>(DefaultStringConverter)
-class CheckBox() : UiNode<Config.BooleanEntry, Boolean>(DefaultBooleanConverter)
+sealed class UiNode<in T : Config.Entry, S, G>(val converter: ConfigValueConverter<T, S, G>)
+class TextBox : UiNode<Config.StringEntry, String?, String?>(DefaultStringConverter)
+class PasswordBox : UiNode<Config.StringEntry, String?, String?>(DefaultStringConverter)
+class CheckBox : UiNode<Config.BooleanEntry, Boolean?, Boolean>(DefaultBooleanConverter)
+class ActionButton(val text: String, val action: () -> Boolean) :
+    UiNode<Config.StringEntry, String?, String?>(DefaultStringConverter)
+
 class NumberBox @JvmOverloads constructor(val min: Int = 0, val max: Int = 100) :
-    UiNode<Config.StringEntry, Int>(object : ConfigValueConverter<Config.StringEntry, Int> {
-      override fun getDefault(t: Config.StringEntry): Int = try {
-        t.defaultValue?.toInt() ?: min
-      } catch (e: NumberFormatException) {
-        min
-      }
+    UiNode<Config.StringEntry, Int, Int>(
+        object : ConfigValueConverter<Config.StringEntry, Int, Int> {
+          override fun getDefault(t: Config.StringEntry): Int = try {
+            t.defaultValue?.toInt() ?: min
+          } catch (e: NumberFormatException) {
+            min
+          }
 
-      override fun getWithoutDefault(t: Config.StringEntry): Int = getWithDefault(t)
+          override fun getWithoutDefault(t: Config.StringEntry): Int = getWithDefault(t)
 
-      override fun getWithDefault(t: Config.StringEntry): Int = try {
-        t.valueWithoutDefault?.toInt() ?: getDefault(t)
-      } catch (e: NumberFormatException) {
-        getDefault(t)
-      }
+          override fun getWithDefault(t: Config.StringEntry): Int = try {
+            t.valueWithoutDefault?.toInt() ?: getDefault(t)
+          } catch (e: NumberFormatException) {
+            getDefault(t)
+          }
 
-      override fun set(t: Config.StringEntry, u: Int) = t.set(u.toString())
-    })
+          override fun set(t: Config.StringEntry, u: Int) = t.set(u.toString())
+        }
+    )
 
-class ChoiceBox @JvmOverloads constructor(val refresh: () -> List<String>?,
-    converter: ConfigValueConverter<Config.StringEntry, String?> = DefaultStringConverter) :
-    UiNode<Config.StringEntry, String?>(converter)
+interface Choice {
 
+  val id: String
+  val displayName: String
+}
 
-class FileChooserButton @JvmOverloads constructor(val isFolder: Boolean = true,
-    converter: ConfigValueConverter<Config.StringEntry, String?> = DefaultStringConverter) :
-    UiNode<Config.StringEntry, String?>(converter)
+class ChoiceBox<out T : Choice> @JvmOverloads constructor(val refresh: () -> List<T>?,
+    val lazy: Boolean = false) :
+    UiNode<Config.StringEntry, String?, String?>(DefaultStringConverter)
+
+class FileChooserButton @JvmOverloads constructor(val isFolder: Boolean = true) :
+    UiNode<Config.StringEntry, String?, String?>(DefaultStringConverter)
