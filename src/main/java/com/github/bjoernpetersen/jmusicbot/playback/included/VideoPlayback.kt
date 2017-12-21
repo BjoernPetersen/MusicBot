9a6b93e674a7a80a81993948bed72797b5d644cd
Björn Petersen <pheasn@gmail.com>
2017-12-21 23:19:37 +0100

    Add video playback interfaces

---
+++ src/main/java/com/github/bjoernpetersen/jmusicbot/playback/included/VideoPlayback.kt
@@ -0,0 +1,8 @@
+package com.github.bjoernpetersen.jmusicbot.playback.included
+
+import com.github.bjoernpetersen.jmusicbot.playback.FilePlaybackFactory
+
+interface MkvPlaybackFactory : FilePlaybackFactory
+interface WmvPlaybackFactory : FilePlaybackFactory
+interface Mp4PlaybackFactory : FilePlaybackFactory
+interface AviPlaybackFactory : FilePlaybackFactory
