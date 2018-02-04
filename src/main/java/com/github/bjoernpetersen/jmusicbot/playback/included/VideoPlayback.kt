9a00e11aebf5e28cb160e98dc7303fd812005308
Björn Petersen <pheasn@gmail.com>
2018-02-04 14:12:01 +0100

    Mark VideoPlayback interfaces as @Beta

--- src/main/java/com/github/bjoernpetersen/jmusicbot/playback/included/VideoPlayback.kt
+++ src/main/java/com/github/bjoernpetersen/jmusicbot/playback/included/VideoPlayback.kt
@@ -1,8 +1,16 @@
 package com.github.bjoernpetersen.jmusicbot.playback.included
 
 import com.github.bjoernpetersen.jmusicbot.playback.FilePlaybackFactory
+import com.google.common.annotations.Beta
 
+@Beta
 interface MkvPlaybackFactory : FilePlaybackFactory
+
+@Beta
 interface WmvPlaybackFactory : FilePlaybackFactory
+
+@Beta
 interface Mp4PlaybackFactory : FilePlaybackFactory
+
+@Beta
 interface AviPlaybackFactory : FilePlaybackFactory
