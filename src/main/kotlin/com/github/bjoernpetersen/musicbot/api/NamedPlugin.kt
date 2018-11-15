14119812713f84a0c2f62e5c37d116d3abb63405
Björn Petersen <pheasn@gmail.com>
2018-11-15 14:11:01 +0100

    Add secondary contructor for NamedPlugin

--- src/main/kotlin/com/github/bjoernpetersen/musicbot/api/NamedPlugin.kt
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/api/NamedPlugin.kt
@@ -3,4 +3,6 @@ package com.github.bjoernpetersen.musicbot.api
 import com.github.bjoernpetersen.musicbot.spi.plugin.Plugin
 import kotlin.reflect.KClass
 
-data class NamedPlugin<out T : Plugin>(val id: KClass<out T>, val name: String)
+data class NamedPlugin<out T : Plugin>(val id: KClass<out T>, val name: String) {
+    constructor(id: Class<out T>, name: String) : this(id.kotlin, name)
+}
