877f11dc3267ac450416095aae88348540d27fe6
Björn Petersen <pheasn@gmail.com>
2018-11-15 13:49:01 +0100

    Add defaults to nullable song props

--- src/main/kotlin/com/github/bjoernpetersen/musicbot/api/Song.kt
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/api/Song.kt
@@ -2,10 +2,11 @@ package com.github.bjoernpetersen.musicbot.api
 
 import com.github.bjoernpetersen.musicbot.spi.plugin.Provider
 
-data class Song(
+
+data class Song @JvmOverloads constructor(
     val id: String,
     val provider: NamedPlugin<Provider>,
     val title: String,
     val description: String,
-    val duration: Int?,
-    val albumArtUrl: String?)
+    val duration: Int? = null,
+    val albumArtUrl: String? = null)
