0236d0f55d5bdb2a63d47e31d573db28f60d6fcb
Björn Petersen <pheasn@gmail.com>
2018-11-17 14:05:18 +0100

    Make ActionButton a UiNode<String>

--- src/main/kotlin/com/github/bjoernpetersen/musicbot/api/config/ConfigUi.kt
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/api/config/ConfigUi.kt
@@ -7,7 +7,7 @@ object TextBox : UiNode<String>()
 object PasswordBox : UiNode<String>()
 object CheckBox : UiNode<Boolean>()
 // TODO sure about that?
-data class ActionButton(val label: String, val action: () -> Boolean) : UiNode<Unit>()
+data class ActionButton(val label: String, val action: () -> Boolean) : UiNode<String>()
 
 data class NumberBox @JvmOverloads constructor(val min: Int = 0, val max: Int = 100) : UiNode<Int>()
 data class FileChooser(val isDirectory: Boolean = true) : UiNode<File>()
