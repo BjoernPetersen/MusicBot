bf34e8a7aaff2be4375c264b5041a93bf984836d
Björn Petersen <pheasn@gmail.com>
2018-11-17 23:22:03 +0100

    Add some docs on Plugin lifecycle

--- src/main/kotlin/com/github/bjoernpetersen/musicbot/spi/plugin/Plugin.kt
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/spi/plugin/Plugin.kt
@@ -3,7 +3,49 @@ package com.github.bjoernpetersen.musicbot.spi.plugin
 import com.github.bjoernpetersen.musicbot.api.config.Config
 import com.github.bjoernpetersen.musicbot.spi.plugin.management.InitStateWriter
 import java.io.IOException
+import javax.inject.Inject
 
+/**
+ * Base interface for plugins. This interface isn't meant to be directly implemented, but to be
+ * extended by a specialized interface first.
+ *
+ * ## Dependencies
+ *
+ * A plugin can request dependencies (typically other plugins) by marking fields with the
+ * [Inject] annotation. Please note that dependency injection in constructors is not possible.
+ *
+ * ## Lifecycle
+ *
+ * A plugin goes through several different lifecycle phases due to the way it is integrated in the
+ * MusicBot.
+ *
+ * ### Creation
+ *
+ * The plugin is loaded from an external Jar-File and instantiated using a no-args constructor.
+ * In this phase, no actual work should be done by the plugin and no resources should be allocated.
+ *
+ * ### Dependency injection
+ *
+ * In a second, **separate** step, the dependencies requested by the plugin are resolved
+ * and injected. There is no notification for the plugin about this.
+ *
+ * ### Configuration
+ *
+ * The plugin is asked to provide its configuration entries by [createConfigEntries] and
+ * [createSecretEntries]. The returned entries are shown to the user and their value may change
+ * any number of times during this phase.
+ *
+ * ### Initialization
+ *
+ * The [initialize] method is called after configuration is done and the plugin may allocate
+ * additional resources for the time it is running.
+ *
+ * ### Destruction
+ *
+ * The [close] method is called and all resources should be released.
+ * The plugin instance will never be used again at this point.
+ *
+ */
 interface Plugin {
 
     /**
