ec4f3f25e82bd0842aa40ac9b5a64aeb99e956cb
Björn Petersen <pheasn@gmail.com>
2018-11-17 13:47:22 +0100

    Add convenience constructor to NamedPlugin

--- src/main/kotlin/com/github/bjoernpetersen/musicbot/api/NamedPlugin.kt
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/api/NamedPlugin.kt
@@ -1,7 +1,9 @@
 package com.github.bjoernpetersen.musicbot.api
 
 import com.github.bjoernpetersen.musicbot.spi.plugin.Plugin
+import kotlin.reflect.KClass
 
 data class NamedPlugin<out T : Plugin>(val id: String, val name: String) {
-    constructor(id: Class<out T>, name: String) : this(id.name, name)
+    constructor(idClass: Class<out T>, name: String) : this(idClass.name, name)
+    constructor(idClass: KClass<out T>, name: String) : this(idClass.java, name)
 }
