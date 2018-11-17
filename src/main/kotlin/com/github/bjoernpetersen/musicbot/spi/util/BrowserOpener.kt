bd199800eaca73678c4e327f1d891fb40f186011
Björn Petersen <pheasn@gmail.com>
2018-11-17 18:07:59 +0100

    Add BrowserOpener interface

---
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/spi/util/BrowserOpener.kt
@@ -0,0 +1,14 @@
+package com.github.bjoernpetersen.musicbot.spi.util
+
+import java.net.URL
+
+/**
+ * An object capable of opening a URL in the user's preferred web browser.
+ */
+interface BrowserOpener {
+
+    /**
+     * Open the specified URL in the user's default web browser.
+     */
+    fun openDocument(url: URL)
+}
