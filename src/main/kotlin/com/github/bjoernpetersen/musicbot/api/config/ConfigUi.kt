909440a20f769620b09cff058e496ded7f682ffb
Björn Petersen <pheasn@gmail.com>
2018-11-17 17:46:31 +0100

    Introduce StringRepresentable interface for Config UI

--- src/main/kotlin/com/github/bjoernpetersen/musicbot/api/config/ConfigUi.kt
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/api/config/ConfigUi.kt
@@ -1,5 +1,6 @@
 package com.github.bjoernpetersen.musicbot.api.config
 
+import com.google.common.annotations.Beta
 import java.io.File
 
 sealed class UiNode<T>
@@ -7,10 +8,20 @@ object TextBox : UiNode<String>()
 object PasswordBox : UiNode<String>()
 object CheckBox : UiNode<Boolean>()
 // TODO sure about that?
-data class ActionButton(val label: String, val action: () -> Boolean) : UiNode<String>()
+@Beta
+data class ActionButton<T : StringRepresentable>(val label: String, val action: () -> Boolean) :
+    UiNode<T>()
 
 data class NumberBox @JvmOverloads constructor(val min: Int = 0, val max: Int = 100) : UiNode<Int>()
 data class FileChooser(val isDirectory: Boolean = true) : UiNode<File>()
-data class ChoiceBox<T> @JvmOverloads constructor(
+data class ChoiceBox<T : StringRepresentable> @JvmOverloads constructor(
     val refresh: () -> List<T>?,
     val lazy: Boolean = false) : UiNode<T>()
+
+/**
+ * An object that is representable as a readable String.
+ */
+interface StringRepresentable {
+
+    val readableString: String
+}
