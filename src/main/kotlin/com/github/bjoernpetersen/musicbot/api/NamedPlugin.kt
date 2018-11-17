71d76f2934c7f0216602d9d61c121d725a0ff252
Björn Petersen <pheasn@gmail.com>
2018-11-17 13:25:57 +0100

    Change NamedPlugin.id type to String

--- src/main/kotlin/com/github/bjoernpetersen/musicbot/api/NamedPlugin.kt
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/api/NamedPlugin.kt
@@ -1,8 +1,7 @@
 package com.github.bjoernpetersen.musicbot.api
 
 import com.github.bjoernpetersen.musicbot.spi.plugin.Plugin
-import kotlin.reflect.KClass
 
-data class NamedPlugin<out T : Plugin>(val id: KClass<out T>, val name: String) {
-    constructor(id: Class<out T>, name: String) : this(id.kotlin, name)
+data class NamedPlugin<out T : Plugin>(val id: String, val name: String) {
+    constructor(id: Class<out T>, name: String) : this(id.name, name)
 }
