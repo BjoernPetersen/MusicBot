d312a5d129171420a3187318ee3bb871341cb02f
Björn Petersen <pheasn@gmail.com>
2018-11-18 18:59:03 +0100

    Let ConfigModule extend AbstractModule

--- src/main/kotlin/com/github/bjoernpetersen/musicbot/api/module/ConfigModule.kt
+++ src/main/kotlin/com/github/bjoernpetersen/musicbot/api/module/ConfigModule.kt
@@ -1,10 +1,11 @@
 package com.github.bjoernpetersen.musicbot.api.module
 
 import com.github.bjoernpetersen.musicbot.api.config.ConfigManager
+import com.google.inject.AbstractModule
 import com.google.inject.Provides
 import javax.inject.Singleton
 
-class ConfigModule(private val configManager: ConfigManager) {
+class ConfigModule(private val configManager: ConfigManager) : AbstractModule() {
     @Provides
     @Singleton
     fun configManager() = configManager
