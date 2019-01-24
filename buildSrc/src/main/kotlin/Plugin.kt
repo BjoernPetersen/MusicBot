dccb02638d1915ed25aa9014bb43e6c4486b64e5
Björn Petersen <pheasn@gmail.com>
2019-01-24 16:10:52 +0100

    Update kotlin version to 1.3.20

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,7 +1,7 @@
 object Plugin {
     const val VERSIONS = "0.20.0"
 
-    const val KOTLIN = "1.3.11"
+    const val KOTLIN = "1.3.20"
     const val DOKKA = "0.9.17"
     const val SPOTBUGS_PLUGIN = "1.6.9"
     const val SPOTBUGS_TOOL = "3.1.10"
