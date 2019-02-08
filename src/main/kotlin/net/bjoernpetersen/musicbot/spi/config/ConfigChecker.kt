9faa6c675ada1ad42d5229a59fd8bae474a83d7c
Björn Petersen <pheasn@gmail.com>
2019-02-08 13:30:25 +0100

    Name input arg for ConfigChecker

--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/config/ConfigChecker.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/config/ConfigChecker.kt
@@ -4,10 +4,10 @@ package net.bjoernpetersen.musicbot.spi.config
  * Checks whether the given value is valid.
  *
  * Note that, if you set a default value for the config entry, the input value for the checker will
- * never be null (or blank, if it's a String).
+ * never be null.
  *
  * @return a warning message, or null
  */
-typealias ConfigChecker<T> = (T?) -> String?
+typealias ConfigChecker<T> = (configValue: T?) -> String?
 
 
