e9112b0ced3ac26b983112224f77d01a1f5b48f2
Björn Petersen <pheasn@gmail.com>
2019-03-21 12:45:22 +0100

    Update lib and plugin versions

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,18 +1,18 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
-    const val SLF4J = "1.8.0-beta2"
-    const val KOTLIN_LOGGING = "1.6.23"
+    const val SLF4J = "1.8.0-beta4"
+    const val KOTLIN_LOGGING = "1.6.25"
     const val KOTLIN_COROUTINES = "1.1.1"
 
-    const val GUAVA = "27.0.1-android"
+    const val GUAVA = "27.1-android"
     const val GUICE = "4.2.2"
-    const val SQLITE = "3.25.2"
+    const val SQLITE = "3.27.2.1"
 
     const val JBCRYPT = "0.4"
-    const val JJWT = "3.7.0"
+    const val JJWT = "3.8.0"
     const val JAVA_SEMVER = "0.9.0"
 
-    const val JUNIT = "5.3.1"
-    const val MOCK_K = "1.9"
-    const val ASSERT_J = "3.11.1"
+    const val JUNIT = "5.4.1"
+    const val MOCK_K = "1.9.2"
+    const val ASSERT_J = "3.12.2"
 }
--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,8 +1,6 @@
 object Plugin {
-    const val VERSIONS = "0.20.0"
+    const val VERSIONS = "0.21.0"
 
     const val KOTLIN = "1.3.21"
-    const val DOKKA = "0.9.17"
-    const val SPOTBUGS_PLUGIN = "1.6.9"
-    const val SPOTBUGS_TOOL = "3.1.11"
+    const val DOKKA = "0.9.18"
 }
