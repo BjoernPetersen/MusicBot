7719b62cb92e3dfacd9b3db4d2b339437053bbcf
Björn Petersen <pheasn@gmail.com>
2019-05-03 21:21:08 +0200

    Remove deprecated functions

--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/loader/ResourceCache.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/loader/ResourceCache.kt
@@ -1,28 +1,11 @@
 package net.bjoernpetersen.musicbot.spi.loader
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.future.asCompletableFuture
-import kotlinx.coroutines.runBlocking
 import net.bjoernpetersen.musicbot.api.player.Song
-import java.util.concurrent.Future
 
 /**
  * Manages resources and actively frees unused resources.
  */
 interface ResourceCache {
-
-    /**
-     * Looks up the resource from the cache, otherwise invokes [SongLoader.startLoading].
-     *
-     * @return a future which eventually contains a resource
-     */
-    @Suppress("DeprecatedCallableAddReplaceWith")
-    @Deprecated("Deprecated for removal in favor of get")
-    fun getFuture(song: Song): Future<Resource> = runBlocking {
-        async {
-            get(song)
-        }.asCompletableFuture()
-    }
 
     /**
      * Looks up the resource from the cache, otherwise invokes [SongLoader.load].
--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/loader/SongLoader.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/loader/SongLoader.kt
@@ -1,30 +1,12 @@
 package net.bjoernpetersen.musicbot.spi.loader
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.future.asCompletableFuture
-import kotlinx.coroutines.runBlocking
 import net.bjoernpetersen.musicbot.api.player.Song
 import net.bjoernpetersen.musicbot.spi.plugin.Provider
-import java.util.concurrent.Future
 
 /**
  * Responsible for loading songs by calling [Provider.loadSong].
  */
 interface SongLoader {
-
-    /**
-     * Asynchronously loads the specified [song] using the specified [provider].
-     *
-     * @param provider a provider capable of loading [song]
-     * @param song a song to be loaded
-     * @return a future eventually yielding the result of [Provider.loadSong]
-     */
-    @Deprecated("Use load instead", replaceWith = ReplaceWith("load"))
-    fun startLoading(provider: Provider, song: Song): Future<Resource> = runBlocking {
-        async {
-            load(provider, song)
-        }.asCompletableFuture()
-    }
 
     /**
      * Loads the specified [song] using the specified [provider].
