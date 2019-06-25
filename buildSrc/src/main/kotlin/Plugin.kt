1bd60ff88ae2c351876e44146adabc10c3e0b3e3
Björn Petersen <pheasn@gmail.com>
2019-06-25 13:40:49 +0200

    Update Spotless and Kotlin versions

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,8 +1,8 @@
 object Plugin {
-    const val SPOTLESS = "3.23.0"
+    const val SPOTLESS = "3.23.1"
 
     const val VERSIONS = "0.21.0"
 
-    const val KOTLIN = "1.3.31"
+    const val KOTLIN = "1.3.40"
     const val DOKKA = "0.9.17"
 }
