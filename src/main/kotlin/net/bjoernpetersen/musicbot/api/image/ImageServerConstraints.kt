6970f68a5a15b73cde9f9db4f3769ff2e303b3ed
Björn Petersen <pheasn@gmail.com>
2019-06-30 15:48:31 +0200

    Move ImageServerContraints to api

--- src/main/kotlin/net/bjoernpetersen/musicbot/api/image/ImageServerConstraints.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/api/image/ImageServerConstraints.kt
@@ -1,22 +1,17 @@
-package net.bjoernpetersen.musicbot.spi.image
+package net.bjoernpetersen.musicbot.api.image
 
 object ImageServerConstraints {
-    /**
-     * The port to bind the API on.
-     */
-    const val PORT = 42946
-
     /**
      * The path to serve (cached) remote images on.
      *
      * The actual full path will be `$REMOTE_PATH/BASE_64_URL`.
      */
-    const val REMOTE_PATH = "/remote"
+    const val REMOTE_PATH = "/image/remote"
 
     /**
      * The path to serve local images on (supplied by plugins).
      *
      * The actual full path will be `$REMOTE_PATH/BASE_64_PROVIDER_ID/BASE_64_SONG_ID`.
      */
-    const val LOCAL_PATH = "/local"
+    const val LOCAL_PATH = "/image/local"
 }
