d51c771cb279a3751b785dcd05e761826121e689
Björn Petersen <pheasn@gmail.com>
2019-07-02 13:33:11 +0200

    Add video playback bases

---
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/plugin/predefined/VideoFilePlaybackFactory.kt
@@ -0,0 +1,34 @@
+package net.bjoernpetersen.musicbot.spi.plugin.predefined
+
+import net.bjoernpetersen.musicbot.api.plugin.Base
+
+@Experimental(Experimental.Level.WARNING)
+annotation class ExperimentalVideoFilePlayback
+
+/**
+ * PlaybackFactory capable of playing `.avi` files.
+ */
+@ExperimentalVideoFilePlayback
+@Base
+interface AviPlaybackFactory : FilePlaybackFactory
+
+/**
+ * PlaybackFactory capable of playing `.mkv` files.
+ */
+@ExperimentalVideoFilePlayback
+@Base
+interface MkvPlaybackFactory : FilePlaybackFactory
+
+/**
+ * PlaybackFactory capable of playing `.mp4` files.
+ */
+@ExperimentalVideoFilePlayback
+@Base
+interface Mp4PlaybackFactory : FilePlaybackFactory
+
+/**
+ * PlaybackFactory capable of playing `.wmv` files.
+ */
+@ExperimentalVideoFilePlayback
+@Base
+interface WmvPlaybackFactory : FilePlaybackFactory
