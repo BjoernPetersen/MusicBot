937a02a65a21dba1e5ad0e463df5b86027741b33
Björn Petersen <pheasn@gmail.com>
2019-07-27 10:07:43 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,7 +1,7 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
     const val SLF4J = "1.8.0-beta4"
-    const val KOTLIN_LOGGING = "1.6.26"
+    const val KOTLIN_LOGGING = "1.7.2"
     const val KOTLIN_COROUTINES = "1.2.2"
 
     const val GUAVA = "28.0-android"
@@ -12,7 +12,7 @@ object Lib {
     const val JJWT = "3.8.1"
     const val JAVA_SEMVER = "0.9.0"
 
-    const val JUNIT = "5.5.0"
+    const val JUNIT = "5.5.1"
     const val JUNIT_GUICE = "1.1.1"
     const val MOCK_K = "1.9.3"
     const val ASSERT_J = "3.12.2"
