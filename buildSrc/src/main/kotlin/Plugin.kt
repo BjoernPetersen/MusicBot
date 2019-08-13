ecd52902b574a3d3bd33e44f4686447b76cd78ba
Björn Petersen <pheasn@gmail.com>
2019-08-13 15:59:50 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,7 +1,7 @@
 object Plugin {
-    const val SPOTLESS = "3.23.1"
+    const val SPOTLESS = "3.24.1"
 
-    const val VERSIONS = "0.21.0"
+    const val VERSIONS = "0.22.0"
 
     const val KOTLIN = "1.3.41"
     const val DOKKA = "0.9.17"
