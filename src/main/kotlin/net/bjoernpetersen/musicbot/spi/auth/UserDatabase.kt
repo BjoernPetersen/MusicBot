24eb1e2d769062fc3a80e07e803bce95a64d4bab
Björn Petersen <pheasn@gmail.com>
2019-08-21 23:45:35 +0200

    Improve UserDatabase interface and document it
    - updatePassword just requires a name, not a user
    - findUser return type is not nullable, an exception is thrown instead
    - updatePassword and updatePermissions can throw a UserNotFoundException

--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/auth/UserDatabase.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/auth/UserDatabase.kt
@@ -3,20 +3,94 @@ package net.bjoernpetersen.musicbot.spi.auth
 import net.bjoernpetersen.musicbot.api.auth.DuplicateUserException
 import net.bjoernpetersen.musicbot.api.auth.FullUser
 import net.bjoernpetersen.musicbot.api.auth.Permission
+import net.bjoernpetersen.musicbot.api.auth.UserNotFoundException
 
+/**
+ * Manages all full users in a database.
+ */
 interface UserDatabase {
-    fun findUser(name: String): FullUser?
+    /**
+     * Finds a specific user by his username.
+     *
+     * The name will be trimmed and converted to lower-case according to the US locale before
+     * checking uniqueness.
+     *
+     * @param name the user's name
+     * @return the user object for that name
+     * @throws UserNotFoundException if no user with that name exists
+     */
+    @Throws(UserNotFoundException::class)
+    fun findUser(name: String): FullUser
 
+    /**
+     * Gets a list of all registered users.
+     */
     fun getUsers(): Set<FullUser>
 
+    /**
+     * Inserts a new user into the database.
+     *
+     * @param user a user to insert into the database
+     * @param hash the user's hashed password
+     * @throws DuplicateUserException if a full user with an equal name already exists
+     */
     @Throws(DuplicateUserException::class)
     fun insertUser(user: FullUser, hash: String)
 
-    fun updatePassword(user: FullUser, hash: String)
+    /**
+     * Updates the password of a user.
+     *
+     * The name will be trimmed and converted to lower-case according to the US locale before
+     * checking uniqueness.
+     *
+     * @param name the name of the user whose password should be updated
+     * @param hash the user's hashed password
+     * @throws UserNotFoundException if there is no such user
+     */
+    fun updatePassword(name: String, hash: String)
 
+    /**
+     * Updates the password of a user.
+     *
+     * The name will be trimmed and converted to lower-case according to the US locale before
+     * checking uniqueness.
+     *
+     * @param user the user whose password should be updated
+     * @param hash the user's hashed password
+     * @throws UserNotFoundException if there is no such user
+     */
+    @Deprecated(
+        "Just supply the user's name instead",
+        ReplaceWith("updatePassword(user.name, hash)")
+    )
+    fun updatePassword(user: FullUser, hash: String) {
+        updatePassword(user.name, hash)
+    }
+
+    /**
+     * Updates the permissions of a user.
+     *
+     * The name will be trimmed and converted to lower-case according to the US locale before
+     * checking uniqueness.
+     *
+     * @param name the user who's password should be updated
+     * @param permissions the full list of the user's new permissions
+     * @throws UserNotFoundException if there is no such user
+     */
     fun updatePermissions(name: String, permissions: Set<Permission>)
 
+    /**
+     * Deletes a user from the database.
+     *
+     * The name will be trimmed and converted to lower-case according to the US locale before
+     * checking uniqueness.
+     *
+     * @param name the user's name
+     */
     fun deleteUser(name: String)
 
+    /**
+     * Releases all resources and renders this object unusable.
+     */
     fun close()
 }
