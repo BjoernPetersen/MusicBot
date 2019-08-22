48954475a14ba2f26a724b3f1f5b3bd70740382c
Björn Petersen <pheasn@gmail.com>
2019-08-22 13:53:09 +0200

    Document Guice modules

--- src/main/kotlin/net/bjoernpetersen/musicbot/api/module/BrowserOpenerModule.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/api/module/BrowserOpenerModule.kt
@@ -5,7 +5,15 @@ import com.google.inject.Provides
 import net.bjoernpetersen.musicbot.spi.util.BrowserOpener
 import javax.inject.Singleton
 
+/**
+ * Guice module which binds an instance of [BrowserOpener].
+ *
+ * @param browserOpener the instance to bind for the [BrowserOpener] key
+ */
 class BrowserOpenerModule(private val browserOpener: BrowserOpener) : AbstractModule() {
+    /**
+     * Supplies a [BrowserOpener].
+     */
     @Provides
     @Singleton
     fun provideOpener(): BrowserOpener = browserOpener
--- src/main/kotlin/net/bjoernpetersen/musicbot/api/module/ConfigModule.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/api/module/ConfigModule.kt
@@ -5,7 +5,15 @@ import com.google.inject.Provides
 import net.bjoernpetersen.musicbot.api.config.ConfigManager
 import javax.inject.Singleton
 
+/**
+ * Guice module which binds an instance of [ConfigManager].
+ *
+ * @param configManager the instance to bind for the [ConfigManager] key.
+ */
 class ConfigModule(private val configManager: ConfigManager) : AbstractModule() {
+    /**
+     * Supplies a [ConfigManager].
+     */
     @Provides
     @Singleton
     fun configManager() = configManager
