d9725b1e6e688d6617c01162a5c63b15de8b6bd3
Björn Petersen <pheasn@gmail.com>
2019-08-22 13:58:47 +0200

    Document SongQueue

--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/player/SongQueue.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/player/SongQueue.kt
@@ -6,18 +6,53 @@ import net.bjoernpetersen.musicbot.api.player.Song
 /**
  * A queue containing instances of [QueueEntry].
  *
- * The queue may never contain duplicates.
+ * The queue may never contain duplicates (multiple entries with the same song).
  */
 interface SongQueue {
 
+    /**
+     * Whether the queue is currently empty.
+     */
     val isEmpty: Boolean
+
+    /**
+     * @return the next QueueEntry which should be played, or null if the queue is empty.
+     */
     fun pop(): QueueEntry?
+
+    /**
+     * Inserts a new entry into the queue
+     * @param entry the new entry
+     */
     fun insert(entry: QueueEntry)
+
+    /**
+     * Removes a song from the queue.
+     * @param song the song remove
+     */
     fun remove(song: Song)
+
+    /**
+     * Clear the queue.
+     */
     fun clear()
+
+    /**
+     * Create a list containing all entries in this queue, in the order in which they would be
+     * returned by [pop].
+     *
+     * The returned list is not backed by this queue, i.e. it's not updated when this queue changes.
+     */
     fun toList(): List<QueueEntry>
 
+    /**
+     * Add a listener which will be notified of any changes in the queue.
+     */
     fun addListener(listener: QueueChangeListener)
+
+    /**
+     * Remove a listener which was previously added by calling [addListener].
+     */
     fun removeListener(listener: QueueChangeListener)
 
     /**
