5e79901c54b18b2319d8f3864003f4e95d055a6c
Björn Petersen <pheasn@gmail.com>
2019-08-26 17:21:22 +0200

    Update detekt to 1.0.1

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,6 +1,6 @@
 object Plugin {
     const val SPOTLESS = "3.24.2"
-    const val DETEKT = "1.0.0"
+    const val DETEKT = "1.0.1"
     const val JACOCO = "0.8.4"
 
     const val VERSIONS = "0.22.0"
