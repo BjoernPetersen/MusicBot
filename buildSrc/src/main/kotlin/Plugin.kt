5f4fec7ead519e5369fcbd52929a9ec368b3471b
Björn Petersen <pheasn@gmail.com>
2019-09-02 13:38:12 +0200

    Update versions plugin

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -3,7 +3,7 @@ object Plugin {
     const val DETEKT = "1.0.1"
     const val JACOCO = "0.8.4"
 
-    const val VERSIONS = "0.22.0"
+    const val VERSIONS = "0.24.0"
 
     const val KOTLIN = "1.3.50"
     const val DOKKA = "0.9.17"
