79c06fd32ae3874bed44d2a432f177097d92aa4c
Björn Petersen <pheasn@gmail.com>
2020-01-29 17:49:48 +0100

    Update dependencies

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,10 +1,10 @@
 object Plugin {
-    const val SPOTLESS = "3.26.0"
-    const val DETEKT = "1.1.1"
+    const val SPOTLESS = "3.27.1"
+    const val DETEKT = "1.5.0"
     const val JACOCO = "0.8.4"
 
     const val VERSIONS = "0.27.0"
 
-    const val KOTLIN = "1.3.60"
+    const val KOTLIN = "1.3.61"
     const val DOKKA = "0.10.0"
 }
