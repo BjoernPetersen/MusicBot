19ed3712f35ae6cdc847a2a6faca33e185022383
Björn Petersen <pheasn@gmail.com>
2020-02-05 16:41:39 +0100

    Update Dokka and Detekt

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,10 +1,10 @@
 object Plugin {
     const val SPOTLESS = "3.27.1"
-    const val DETEKT = "1.5.0"
+    const val DETEKT = "1.5.1"
     const val JACOCO = "0.8.4"
 
     const val VERSIONS = "0.27.0"
 
     const val KOTLIN = "1.3.61"
-    const val DOKKA = "0.10.0"
+    const val DOKKA = "0.10.1"
 }
