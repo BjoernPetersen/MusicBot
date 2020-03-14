6ed6fe1bd8036db486ef1aa81f006cdb882418ea
Björn Petersen <pheasn@gmail.com>
2020-03-14 15:51:40 +0100

    Opt-in to OptIn

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,6 +100,7 @@ tasks {
             jvmTarget = "1.8"
             freeCompilerArgs = listOf(
                 "-Xuse-experimental=kotlin.Experimental",
+                "-Xopt-in=kotlin.RequiresOptIn",
                 "-Xnew-inference"
             )
         }
