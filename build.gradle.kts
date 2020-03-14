8044f5f911d0311110738db3e3d0393252a898b1
Björn Petersen <pheasn@gmail.com>
2020-03-14 15:49:23 +0100

    Fix build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,6 @@ tasks {
             jvmTarget = "1.8"
             freeCompilerArgs = listOf(
                 "-Xuse-experimental=kotlin.Experimental",
-                "-Xopt-in=org.mylibrary.OptInAnnotation",
                 "-Xnew-inference"
             )
         }
