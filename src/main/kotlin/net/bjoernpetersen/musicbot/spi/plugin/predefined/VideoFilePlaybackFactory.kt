615b9ef18f903bf9dd8ed71b61662a2462aadfc8
Björn Petersen <pheasn@gmail.com>
2020-03-14 15:38:45 +0100

    Remove experimental annotation on VideoFilePlaybackFactory

--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/plugin/predefined/VideoFilePlaybackFactory.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/plugin/predefined/VideoFilePlaybackFactory.kt
@@ -6,32 +6,29 @@ import net.bjoernpetersen.musicbot.api.plugin.Base
  * Marks all video-playback-related interfaces as experimental.
  */
 @Experimental(Experimental.Level.WARNING)
+@Deprecated("No longer experimental", ReplaceWith(""))
 annotation class ExperimentalVideoFilePlayback
 
 /**
  * PlaybackFactory capable of playing `.avi` files.
  */
-@ExperimentalVideoFilePlayback
 @Base
 interface AviPlaybackFactory : FilePlaybackFactory
 
 /**
  * PlaybackFactory capable of playing `.mkv` files.
  */
-@ExperimentalVideoFilePlayback
 @Base
 interface MkvPlaybackFactory : FilePlaybackFactory
 
 /**
  * PlaybackFactory capable of playing `.mp4` files.
  */
-@ExperimentalVideoFilePlayback
 @Base
 interface Mp4PlaybackFactory : FilePlaybackFactory
 
 /**
  * PlaybackFactory capable of playing `.wmv` files.
  */
-@ExperimentalVideoFilePlayback
 @Base
 interface WmvPlaybackFactory : FilePlaybackFactory
