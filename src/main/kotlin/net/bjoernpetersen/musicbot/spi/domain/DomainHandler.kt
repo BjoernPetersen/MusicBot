796b9250f281c0a9b717efbef9781e7a92f37179
Felix Gail <felix.clemens.gail@gmail.com>
2020-07-15 16:23:15 +0200

    Resolve change requests

--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/domain/DomainHandler.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/domain/DomainHandler.kt
@@ -1,12 +1,12 @@
 package net.bjoernpetersen.musicbot.spi.domain
 
 /**
- * Keeps track of IpAddresses and Domains the MusicBot is accessible by.
+ * Keeps track of Ip Addresses and domains the MusicBot is accessible by.
  */
 interface DomainHandler {
     /**
-     * Returns a map of IpAddress strings to domain names.
+     * Returns a map of Ip address strings to domain names.
      * Used to register the MusicBot at a registry.
      */
-    val ipDomains: Map<String, String>
+    fun getDomainByIp(): Map<String, String>
 }
--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/version/Version.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/version/Version.kt
@@ -1,25 +1,46 @@
 package net.bjoernpetersen.musicbot.spi.version
 
 /**
- * Constains version information about the MusicBot instance
+ * Contains version information about the MusicBot instance
  */
-interface Version {
+data class Version (
     /**
-     * Return the VersionInfo
+     * Version information about the bot
      */
     val versionInfo: VersionInfo
-}
+)
 
 /**
  * Contains information about API version, name of the MusicBot instance and implementation information
  */
 data class VersionInfo(
+    /**
+     * The API version
+     */
     val apiVersion: String,
+    /**
+     * A user-friendly name for the bot instance. May be customized per instance.
+     */
     val botName: String,
+    /**
+     * Information about the implementation serving the API.
+     */
     val implementation: ImplementationInfo
 )
 
 /**
  * Implementation information about the MusicBot
  */
-data class ImplementationInfo(val projectInfo: String, val name: String, val version: String)
+data class ImplementationInfo(
+    /**
+     * The name of the server implementation.
+     */
+    val projectInfo: String,
+    /**
+     * The version of the implementation.
+     */
+    val name: String,
+    /**
+     * URL to the project website.
+     */
+    val version: String)
