9c766e96eb34540ca14c67585904a3367fe2d87d
Felix Gail <felix.clemens.gail@gmail.com>
2020-11-07 20:36:45 +0100

    Update src/main/kotlin/net/bjoernpetersen/musicbot/spi/domain/DomainHandler.kt
    Co-authored-by: Björn Petersen <pheasn@gmail.com>

--- src/main/kotlin/net/bjoernpetersen/musicbot/spi/domain/DomainHandler.kt
+++ src/main/kotlin/net/bjoernpetersen/musicbot/spi/domain/DomainHandler.kt
@@ -5,7 +5,7 @@ package net.bjoernpetersen.musicbot.spi.domain
  */
 interface DomainHandler {
     /**
-     * Returns a map of Ip address strings to domain names.
+     * Returns a map of IP address strings to domain names.
      * Used to register the MusicBot at a registry.
      */
     fun getDomainByIp(): Map<String, String>
