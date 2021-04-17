7d4b3eab82794c27480f80a7c39a21cfcbfed663
Björn Petersen <git@bjoernpetersen.net>
2021-04-17 14:01:07 +0200

    Update Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     kotlin("jvm") version "1.4.32"
     `java-library`
 
-    id("org.jetbrains.dokka") version "1.4.10.2"
+    id("org.jetbrains.dokka") version "1.4.30"
     idea
 
     signing
